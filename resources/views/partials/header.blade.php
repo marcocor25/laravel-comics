63ba05c2f10c98966d7e61d1e2dd06ee4be9853a
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-04-19 19:06:56 +0200

    Inserito header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,42 @@
+<div class="container-fluid">
+    <div class="container">
+        <figure class="logo">
+            <img src="{{ asset('img/dc-logo.png') }}" alt="">
+        </figure>
+
+        <div class="nav-link">
+            <ul>
+                <li>
+                    <a href="/">characters</a>
+                </li>
+                <li>
+                    <a href="/">comics</a>
+                </li>
+                <li>
+                    <a href="/">movies</a>
+                </li>
+                <li>
+                    <a href="/">tv</a>
+                </li>
+                <li>
+                    <a href="/">games</a>
+                </li>
+                <li>
+                    <a href="/">collectibles</a>
+                </li>
+                <li>
+                    <a href="/">videos</a>
+                </li>
+                <li>
+                    <a href="/">fans</a>
+                </li>
+                <li>
+                    <a href="/">news</a>
+                </li>
+                <li>
+                    <a href="/">shop</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
