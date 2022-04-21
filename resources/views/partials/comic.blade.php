d8ac8fe8d08eea2ce94678ff315e74fcf5ab2d6b
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-04-21 21:54:59 +0200

    Esercizio completato (molto minimal ahahah)

--- resources/views/partials/comic.blade.php
+++ resources/views/partials/comic.blade.php
@@ -1,23 +1,24 @@
 @extends('layouts.standard')
 
-@section('metaTitle', 'DC Comic | '. $comic['title'])
+@section('metaTitle', 'DC Comic | ' . $comic['title'])
 
 @section('mainContent')
-    <div class="main-fluid">
+    <div class="main-fluid comic-fluid">
         <div class="jumbotron"></div>
         <div class="container">
-            <div class="badge">
-                <h2>current series</h2>
-            </div>
-
-            <a href="#" class="card">
+            <div class="badge badge-comic">
                 <figure class="card-image">
                     <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
                 </figure>
-                <h5 class="card-title">
-                    {{$comic['title']}}
-                </h5>
-            </a>
+            </div>
+
+            <h1 class="comic-title">
+                {{ $comic['title'] }}
+            </h1>
+
+            <p class="description">
+                {{ $comic['description'] }}
+            </p>
 
         </div>
 
